5484d9b3e9b4b776d1821754fcc13a94f0e0f8d1
Maty Tsoraro <maty.tsoraro@gmail.com>
2022-05-06 12:00:52 +0300

    sdf

--- scripts/index.js
+++ scripts/index.js
@@ -47,6 +47,7 @@ const profileModalFormDescription = profileModalForm.querySelector(
 // Buttons and other DOM elements
 const addCardButton = document.querySelector(".profile__add-button");
 const addCardModalCloseButton = document.querySelector(".popup_close_add-card");
+const previewModalCloseButton = document.querySelector(".popup_close_preview");
 const profileModalCloseButton = document.querySelector(".popup_close_profile");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileTitle = document.querySelector(".profile__title");
@@ -75,7 +76,15 @@ function formSubmitHandler(evt) {
   toggleModalWindow(profileModal);
 }
 
-const onLikeButtonClick = () => {};
+const onImagePreview = (card) => {
+  const popupImage = previewModal.querySelector(".popup__image");
+  popupImage.src = card.link;
+  toggleModalWindow(previewModal);
+};
+
+const onLikeButtonClick = () => {
+  console.log("like");
+};
 
 function createCardElement(card) {
   const cardTemplate = document
@@ -91,10 +100,23 @@ function createCardElement(card) {
 
   cardTitle.textContent = card.name;
 
-  cardImage.addEventListener("click", onLikeButtonClick);
+  cardImage.addEventListener("click", () => onImagePreview(card));
   //handle image click
+  // cardElement
+  // .querySelector(".card__like-button")
+  // .addEventListener("click", (evt) => {
+  //   evt.target.classList.toggle("card__like-button_active");
+  // });
+  // cardElement
+  // .querySelector(".card__removeButton")
+  // .addEventListener("click", () => {
+  //   const thisCard = cardElement.closest(".card");
+  //   thisCard.remove();
+  // });
 
-  cardLikeButton.addEventListener("click", () => onLikeButtonClick(card));
+  cardLikeButton.addEventListener("click", () =>
+    onLikeButtonClick(cardElement)
+  );
 
   return cardElement;
 }
@@ -110,6 +132,9 @@ function renderCard(card, wrapper) {
 addCardButton.addEventListener("click", () => toggleModalWindow(addCardModal));
 addCardModalCloseButton.addEventListener("click", () =>
   toggleModalWindow(addCardModal)
+);
+previewModalCloseButton.addEventListener("click", () =>
+  toggleModalWindow(previewModal)
 );
 profileModalForm.addEventListener("submit", formSubmitHandler);
 profileEditButton.addEventListener("click", () =>
