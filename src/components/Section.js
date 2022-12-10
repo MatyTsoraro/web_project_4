14c28e4fff62f04555204437f29d3bb67cbcb14b
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2022-12-10 23:43:36 +0000

    fixing

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,21 @@
 export default class Section {
     constructor({ items, renderer }, containerSelector) {
-        this._renderedItems = items;
+        this._items = items;
         this._renderer = renderer;
 
         this._container = document.querySelector(containerSelector);
     }
 
     addItem(item) {
-        const card = this._renderer(item);
-        this._container.append(card);
+        const element = this._renderer(item);
+        this._container.append(element);
     }
     prepenedItem(item) {
         this._container.prepend(item);
     }
 
     renderItems() {
-        this._renderedItems.forEach((item) => {
+        this._items.forEach((item) => {
             this.addItem(item);
         });
     }
